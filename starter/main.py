0c7a87a5fc13dd6a8561b84db110aedf63785d93
dangstan <dangstan@github.com>
2023-05-05 10:26:09 -0300

    test file updated

--- starter/main.py
+++ starter/main.py
@@ -17,7 +17,7 @@ async def say_hello():
 
 
 # Loading in model from serialized .pkl file
-pkl_filename = "model/gridxgb_model.pkl"
+pkl_filename = "starter/model/gridxgb_model.pkl"
 with open(pkl_filename, 'rb') as file:
 	model = pickle.load(file)
 
