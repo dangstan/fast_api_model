1c823f3e15eb5aff5ece63053c80e86c69a43156
dangstan <dangstan@github.com>
2023-05-05 11:14:29 -0300

    test_model update 8

--- starter/starter/ml/test_model.py
+++ starter/starter/ml/test_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import scipy.stats
-from starter import compute_model_metrics,inference
+from ml import compute_model_metrics,inference
 
 
 def test_column_names(data):
